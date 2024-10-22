a25394c8e349d07b73b4339426244335fe88d790
Fadil Hisyam <ichorvfx@gmail.com>
2024-10-22 15:04:02 +0700

    add drizzle config

---
+++ drizzle.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from "drizzle-kit";
+ 
+export default defineConfig({
+    dialect: "sqlite",
+    schema: "./db/schema.ts",
+    casing: "snake_case",
+    dbCredentials: { url: "./sqlite.db" }
+});
