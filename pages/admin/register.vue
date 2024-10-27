c29fd2cfa7623b6feb9bd5ad15989557099d7d66
Fadil Hisyam <ichorvfx@gmail.com>
2024-10-27 10:48:23 +0700

    Modify Register page to use FormInputField wrapper and Add onSubmit logic for registering

--- pages/admin/register.vue
+++ pages/admin/register.vue
@@ -1,5 +1,5 @@
 <template>
-  <main class="bg-indigo-50 min-h-screen grid place-items-center px-[14%] py-16"> 
+  <main class="bg-indigo-50 min-h-screen grid place-items-center px-[14%] py-8"> 
     <Card class="m-1 overflow-hidden max-w-4xl w-full">
       <CardContent class="flex p-0">
         <section class="admin-bg bg-[90%_20%] flex flex-col justify-end px-8 py-12 w-1/2 bg-cover">
@@ -12,56 +12,43 @@
           <h2 class="text-xl font-bold text-primary-text mb-6">
             Register Account
           </h2>
-          <form class="flex flex-col gap-4" @submit="onSubmit">
-            <FormField v-slot="{ componentField }" name="name">
-              <FormItem>
-                <FormLabel class="text-primary-text">Full Name</FormLabel>
-                <FormControl>
-                  <Input type="text" placeholder="John Doe" v-bind="componentField" />
-                </FormControl>
-                <FormDescription>This is your public display name.</FormDescription>
-                <FormMessage />
-              </FormItem>
-            </FormField>
-            <FormField v-slot="{ componentField }" name="username">
-              <FormItem>
-                <FormLabel class="text-primary-text">Username</FormLabel>
-                <FormControl>
-                  <Input type="text" placeholder="johndoe01" v-bind="componentField" />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            </FormField>
-            <FormField v-slot="{ componentField }" name="email">
-              <FormItem>
-                <FormLabel class="text-primary-text">Email</FormLabel>
-                <FormControl>
-                  <Input type="email" placeholder="johndoe@example.com" v-bind="componentField" />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            </FormField>
-            <FormField v-slot="{ componentField }" name="password">
-              <FormItem>
-                <FormLabel class="text-primary-text">Password</FormLabel>
-                <FormControl>
-                  <Input type="password" v-bind="componentField" />
-                </FormControl>
-                <FormDescription>Passwords must contain at least six characters</FormDescription>
-                <FormMessage />
-              </FormItem>
-            </FormField>
-            <FormField v-slot="{ componentField }" name="confirmPassword">
-              <FormItem>
-                <FormLabel class="text-primary-text">Confirm Password</FormLabel>
-                <FormControl>
-                  <Input type="password" required v-bind="componentField" />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            </FormField>
+          <form class="flex flex-col gap-6" @submit="onSubmit">
+            <FormInputField
+              name="name"
+              label="Full Name"
+              description="This is your public display name."
+              placeholder="John Doe"
+              is-required
+            />
+            <FormInputField
+              name="username"
+              label="Username"
+              placeholder="johndoe01"
+              is-required
+            />
+            <FormInputField
+              name="email"
+              type="email"
+              label="Email"
+              placeholder="johndoe@example.com"
+              is-required
+            />
+            <FormInputField
+              name="password"
+              type="password"
+              label="Password"
+              description="Passwords must contain at least six characters."
+              is-required
+            />
+            <FormInputField
+              name="confirmPassword"
+              type="password"
+              label="Confirm Password"
+              is-required
+            />
             <div class="mt-4 -mb-2 flex flex-col gap-2">
-              <Button type="submit">
+              <Button :disabled="isLoading" type="submit">
+                <Loader2 v-if="isLoading" class="w-4 h-4 mr-2 animate-spin" />
                 Register
               </Button>
               <Button variant="link" as-child>
@@ -78,19 +65,12 @@
 <script setup lang="ts">
 import { Button } from '@/components/ui/button'
 import {
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
+  FormInputField
 } from '@/components/ui/form'
-import { Input } from '@/components/ui/input'
 import { toast } from '@/components/ui/toast'
-
 import { toTypedSchema } from '@vee-validate/zod'
+import { Loader2 } from 'lucide-vue-next'
 import { useForm } from 'vee-validate'
-import { h } from 'vue'
 import * as z from 'zod'
 
 definePageMeta({
@@ -102,6 +82,8 @@ useSeoMeta({
   ogTitle: 'Register Account - Instant collaboration for remote teams',
 })
 
+const isLoading = ref(false)
+
 const formSchema = toTypedSchema(z.object({
   name: z.string().min(2, "Name must be at least 2 characters long."),
   username: z.string().min(3, "Username must be at least 3 characters long.")
@@ -110,7 +92,7 @@ const formSchema = toTypedSchema(z.object({
   password: z.string().min(6, "Please enter a valid password"),
   confirmPassword: z.string()
 }).refine((data) => data.password === data.confirmPassword, {
-  path: ['confirmPassword'], // specify which field the error should apply to
+  path: ['confirmPassword'],
   message: "Passwords do not match.",
 }))
 
@@ -118,11 +100,38 @@ const { handleSubmit } = useForm({
   validationSchema: formSchema,
 })
 
-const onSubmit = handleSubmit((values) => {
-  toast({
-    title: "Let's go! Login successful.",
-    description: 'Welcome to the admin dashboard.',
-  })
+const onSubmit = handleSubmit(async (values) => {
+  isLoading.value = true
+
+  try {
+    const { email, username, name, password } = values
+
+    await $fetch('/api/register', {
+      method: 'post',
+      body: {
+        email,
+        username,
+        name,
+        password
+      }
+    })
+
+    navigateTo(`/admin/login?_email=${email}`)
+    
+    toast({
+      title: "Registration Successful!",
+      description: `Hello ${name}. Your account has been created, you can login now.`,
+      variant: "success",
+    })
+  } catch (error) {
+    toast({
+      title: "Registration Failed!",
+      description: 'Something happened, please try again later.',
+      variant: "destructive",
+    })
+  }
+
+  isLoading.value = false
 })
 
 </script>
